29eb39a9074af5ee448ae24a7f767a3639ba3da8
Kessler <ak113714@bna.com>
2020-03-25 14:06:29 -0400

    add code for combining waiters and bartenders together, exporting

--- 99_bartendersandwaitstaffcombined.R
+++ 99_bartendersandwaitstaffcombined.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 library(janitor)
 library(readxl)
@@ -19,13 +18,39 @@ msa_allrecs <- msa_allrecs %>%
   filter(!prim_state %in% c("GU", "VI", "PR"))
 
 
+#pull together data for just waiter/waitresses and bartenders ####
 
-# waiters
-state_waiters <- state_foodserv_all %>% 
-  filter(occ_code == "35-3031")  %>% 
+state_wait_bart <- states_allrecs %>% 
+  filter(occ_code %in% c("35-3031", "35-3011")) %>% 
   select(state, occ_title, jobs_1000)
 
-# bartenders
-state_bartenders <- state_foodserv_all %>% 
-  filter(occ_code == "35-3011") %>% 
-  select(state, occ_title, jobs_1000)
+#sum to get total combined per 1000 figures
+combjob100_state_wait_bart <- state_wait_bart %>% 
+  group_by(state) %>% 
+  summarise(jobs_1000_combined = sum(jobs_1000)) %>% 
+  arrange(desc(jobs_1000_combined))
+
+combjob100_state_wait_bart
+
+
+
+## now we'll do it for MSAs ####
+
+msa_wait_bart <- msa_allrecs %>% 
+  filter(occ_code %in% c("35-3031", "35-3011")) %>% 
+  select(area_name, occ_title, jobs_1000)
+
+#sum to get total combined per 1000 figures
+combjob100_msa_wait_bart <- msa_wait_bart %>% 
+  group_by(area_name) %>% 
+  summarise(jobs_1000_combined = sum(jobs_1000)) %>% 
+  arrange(desc(jobs_1000_combined))
+
+combjob100_msa_wait_bart
+
+
+#save to Excel for sharing
+sheets <- list("by STATE" = combjob100_state_wait_bart,
+               "by MSA" = combjob100_msa_wait_bart)
+
+write_xlsx(sheets, "output/wait_bart_combined.xlsx")
